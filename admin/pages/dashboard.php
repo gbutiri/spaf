40fb52bf29c8d212878448bc900cff1033205c1c
George Butiri <MovieMaker713@gmail.com>
2015-05-05 16:04:43 -0700

    Made some pretty form elements work with font awesome.

--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -1,25 +1,64 @@
 <?php include (_DOCROOT . '/htmls/admin-header.php'); ?>
 
-<h1>Dashboard Page</h1>
 
-<p>
-Test Ajax OK Modal.
-<a href="#" class="tmbtn" data-action="show_test_modal" data-module="dashboard">Do something.</a>
-</p>
+<div class="row">
+	<div class="col-md-3">
+	</div>
+	<div class="col-md-6">
+		<h1>Dashboard Page</h1>
 
-<p>
-Test Ajax Confirm Modal. 
-<a href="#" class="tmbtn" data-action="show_test_modal_confirm" data-module="dashboard">Remove STEVE</a>
-</p>
+		<p>
+		Test Ajax OK Modal.
+		<a href="#" class="tmbtn" data-action="show_test_modal" data-module="dashboard">Do something.</a>
+		</p>
 
-<div id="steve_holder">
-	<div id="steve">STEVE</div>
+		<p>
+		Test Ajax Confirm Modal. 
+		<a href="#" class="tmbtn" data-action="show_test_modal_confirm" data-module="dashboard">Remove STEVE</a>
+		</p>
+
+		<div id="steve_holder">
+			<div id="steve">STEVE</div>
+		</div>
+
+		<p>
+		Bring Steve Back: 
+		<a href="#" class="tmbtn" data-action="bring_steve_back" data-module="dashboard">Bring Steve Back</a>
+		</p>
+	
+		<input type="text" class="form-control">
+		
+		
+		<label class="checkbox" for="checkbox2">
+            <input type="checkbox" value="" id="checkbox2" data-toggle="checkbox" class="custom-checkbox">
+			<span class="icons">
+				<span class="fa fa-check-square"></span>
+				<span class="fa fa-square"></span>
+			</span>
+            Value
+		</label>		
+		
+		
+		
+		<label class="radio">
+            <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" data-toggle="radio" class="custom-radio" />
+			<span class="icons" >
+				<span class="fa fa-circle-o"></span>
+				<span class="fa fa-dot-circle-o"></span>
+			</span>
+            Radio is off
+          </label>
+		<label class="radio">
+            <input type="radio" name="optionsRadios" id="optionsRadios2" value="option1" data-toggle="radio" checked="" class="custom-radio" />
+			<span class="icons">
+				<span class="fa fa-circle-o"></span>
+				<span class="fa fa-dot-circle-o"></span>
+			</span>
+            Radio is on
+          </label>
+	</div>
+	<div class="col-md-3">
+	</div>
 </div>
-
-<p>
-Bring Steve Back: 
-<a href="#" class="tmbtn" data-action="bring_steve_back" data-module="dashboard">Bring Steve Back</a>
-</p>
-
 
 <?php include (_DOCROOT . '/htmls/admin-footer.php'); ?>
--- htmls/header.php
+++ htmls/header.php
@@ -1,11 +1,13 @@
 <html>
 
 <head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="/css/ajax-controller.css" />
 	<link rel="stylesheet" href="/css/flat-ui.min.css" />
 	<link rel="stylesheet" href="/css/main.css" />
 	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<script src="/js/ajax-controller.js"></script>
 	
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
