90beae84a4266cffb0232df1e17608dc348af18b
george <MovieMaker713@gmail.com>
2015-05-05 09:02:54 -0700

    added the reference to ajax controllers in index file.

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<h1>Index Marketing Page</h1>
+<html>
+
+<head>
+	<link rel="stylesheet" href="/css/ajax-controller.css" />
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="/js/ajax-controller.js"></script>
+</head>
+
+
+
+<body>
+	<h1>Index Marketing Page</h1>
+</body>
+
+
+</html>
