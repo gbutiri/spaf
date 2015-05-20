df82f2c7611e96be9d9427049317e40aa538c378
George Butiri <MovieMaker713@gmail.com>
2015-05-20 15:24:00 -0700

    Renamed the admin folder.

--- admin/pages/mail.php
+++
@@ -1,9 +0,0 @@
-<?php include (_DOCROOT . '/htmls/admin-header.php'); ?>
-
-<h1>Mail Page</h1>
-
-<a href="#" class="tmbtn" data-action="show_test_modal" data-module="mail">Test Ajax</a>
-
-
-
-<?php include (_DOCROOT . '/htmls/admin-footer.php'); ?>
---
+++ modules/pages/mail.php
@@ -0,0 +1,17 @@
+<?php include (_DOCROOT . '/htmls/admin-header.php'); ?>
+
+
+<div class="container">
+	<div class="row">
+
+		<div class="col-md-12">
+			<h1>Mail Page</h1>
+
+			<a href="#" class="tmbtn" data-action="show_test_modal" data-module="mail">Test Ajax</a>
+
+		</div>
+	</div>
+</div>
+
+
+<?php include (_DOCROOT . '/htmls/admin-footer.php'); ?>
